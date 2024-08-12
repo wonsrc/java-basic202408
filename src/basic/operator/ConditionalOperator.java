3809b1b00aaffbff493cb7600e00dca743547281
wonsrc <wonsrc@gmail.com>
2024-08-12 12:00:45 +0900

    Day-02 - operator

---
+++ src/basic/operator/ConditionalOperator.java
@@ -0,0 +1,41 @@
+package basic.operator;
+
+public class ConditionalOperator {
+    public static void main(String[] args) {
+        /*
+         # 3항 연산자
+         - 피 연산자(연산을 당하는 값)가 3개인 연산자를 말합니다.
+         - (조건식 ? 좌항 : 우항);
+         - 조건식을 비교하여 true일 경우에는 좌항이,
+          false일 경우에는 우항의 값이 도출됩니다.
+        */
+
+        int x = 10, y = 20;
+
+        String result = x < y ? "x는 y보다 큽니다" : "x는 y보다 작습니다.";
+        System.out.println("result = " + result);
+
+        // 연산자의 우선순위
+        // 괄호 -> 참조(.) -> 단항 -> 이항 -> 삼항 -> 대입
+
+        // 난수(랜덤값)을 발생시키는 베서드 Math.random();
+        // 0.0 이상 1.0 미만의 실수 난수값을 반환해 줍니다. 0.0 <= x < 1.0
+
+
+        // *** 범위 ***
+        // 1. 뒷범위 먼저 맞춰준다.
+        // 2. 앞자리 맞춰줄때는 더하기
+        // 곱하기 할때 앞자리 만큼 덜 곱해주면 범위 완성
+
+        double rn = Math.random();
+        System.out.println("rn = " + rn);
+
+        // 1 ~ 10 까지의 정수 난수를 발생.
+        int rn2 = (int) ((Math.random() * 10) + 1);
+        System.out.println("rn2 = " + rn2);
+
+        // 10 ~ 100 까지의 정수 난수를 발생
+        int rn3 =(int) (Math.random() * 91) + 10;
+        System.out.println("rn3 = " + rn3);
+    }
+}
