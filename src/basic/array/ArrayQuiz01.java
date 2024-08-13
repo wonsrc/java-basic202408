c6f20e8df7049fb82f75522a86954377fbdf0540
wonsrc <wonsrc@gmail.com>
2024-08-14 00:05:09 +0900

    Array Quiz 풀어보기

--- src/basic/array/ArrayQuiz01.java
+++ src/basic/array/ArrayQuiz01.java
@@ -1,16 +1,46 @@
 package basic.array;
 
+import javax.lang.model.SourceVersion;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class ArrayQuiz01 {
     public static void main(String[] args) {
         /*
             # 음식명을 입력받아서 배열에 순서대로 저장하는 코드
-            1. 음식명을 입력받는다.
+            1. 음식명을 입력받는다.ㅇ
             2. 음식명에 '그만'이 입력되었는가?
-            2-1. 그만이 입력되었으면 종료한다.
+            2-1. 그만이 입력되었으면 종료한다.ㅇ
             2-2. 그만이 입력되지 않았으면 배열에 넣는다.
             3. 배열의 크기를 1 증가시킨다.
             4. 기존의 음식데이터를 증가된 새 배열에 복사한다.
             5. 새로운 음식명을 마지막 위치에 추가한다.
          */
+        Scanner sc = new Scanner(System.in);
+        int first = 1;
+        String [] foodMenu = new String[]{"first"};
+        while(true) {
+            System.out.println("음식을 입력해주세요");
+            String food = sc.next();
+            if(food.equals("stop")) break;
+            if(first == 1) {
+                foodMenu[0] = food;
+                first = -1;
+                System.out.println(Arrays.toString(foodMenu));
+            } else {
+                String [] tmp = new String[foodMenu.length +1];
+
+                for(int i = 0; i < foodMenu.length; i++) {
+                    tmp[i] = foodMenu[i];
+                }
+
+                tmp[tmp.length-1] = food;
+
+                foodMenu = tmp; tmp = null;
+
+                System.out.println(Arrays.toString(foodMenu));
+            }
+        }
+
     }
 }
--- src/basic/array/ArrayQuiz02.java
+++ src/basic/array/ArrayQuiz02.java
@@ -3,13 +3,7 @@ package basic.array;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class ArrayQuiz02 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String[] kakao = {"무지", "네오", "어피치", "라이언", "춘식이"};
-        System.out.println("현재 저장된 친구들 : " + Arrays.toString(kakao));
-
-        /*
+    /*
          1. 삭제할 친구를 입력받아서 삭제를 진행해 주세요.
 
          2. 입력받은 이름이 없다면 없다고 얘기해 주세요.
@@ -18,5 +12,52 @@ public class ArrayQuiz02 {
          배열의 길이가 0이 될 때까지 삭제를 반복해 보세요.
          더 이상 지울 친구가 없다면 프로그램 종료.
          */
+
+
+/*
+입력받은 문자열이랑 기존 문자열이랑 비교해서 target 을 받고 delete 진행
+target은 -1로 초기화해서 값이 변화 없으면 없는 이라고 하고 continue
+길이가 0 되면 종료
+ */
+public class ArrayQuiz02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] kakao = {"MZ", "NO", "APC", "RYN", "CHS"};
+        System.out.println("현재 저장된 친구들 : " + Arrays.toString(kakao));
+
+
+        while(kakao.length != 0) {
+            System.out.print("친구의 이름을 지워주세요: ");
+            String delete = sc.next();
+
+            int target = -1;
+            for(int i = 0; i < kakao.length; i++) {
+                if(delete.equals(kakao[i])) {
+                    target = i;
+                    break;
+                }
+            }
+            if(target == -1) {
+                System.out.println(delete + "은(는)없는 이름 입니다.");
+                continue;
+            }
+
+            for(int i = target; i < kakao.length -1; i++) {
+                kakao[i] = kakao[i+1];
+            }
+
+            String[] temp = new String[kakao.length-1];
+
+            for(int i = 0; i < temp.length; i++) {
+                temp[i] = kakao[i];
+            }
+
+            kakao = temp; temp = null;
+
+            System.out.println("현재 저장된 친구들 : " + Arrays.toString(kakao));
+
+        }
+        sc.close();
+
     }
 }
